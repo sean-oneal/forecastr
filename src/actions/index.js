b8ae978d78fe6dc1841277ce5c4f971b82e12057
Sean O'Neal <17163675+sean-oneal@users.noreply.github.com>
2018-07-26 17:42:20 -0700

    Connect Axios to Actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,11 @@
+import axios from 'axios';
 
 const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${process.env.API_KEY}`;
 
 export FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
+
   const url = `${ROOT_URL}&q=${city}, us`;
   const request = axios.get(url);
 

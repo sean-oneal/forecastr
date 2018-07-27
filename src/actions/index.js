0322cefc8a9f5ccbb42e65de10cbc1a273252224
Sean O'Neal <17163675+sean-oneal@users.noreply.github.com>
2018-07-26 17:41:18 -0700

    Create HTTP Request, Fetch City Weather

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,8 +5,10 @@ export FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city}, us`;
+  const request = axios.get(url);
 
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   };
 }

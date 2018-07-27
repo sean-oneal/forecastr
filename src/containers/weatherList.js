6349b07d984a534a2d18fcabd7bb78cda3f7ba29
Sean O'Neal <17163675+sean-oneal@users.noreply.github.com>
2018-07-26 18:37:00 -0700

    Generate Table Rows For City Data in List

--- src/containers/weatherList.js
+++ src/containers/weatherList.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class WeatherList extends Component {
+  renderWeather({ city }) {
+    const {name} = city;
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+      </tr>
+    );
+  }
   render(){
     return (
       <table className="table table-hover">
@@ -14,6 +22,7 @@ class WeatherList extends Component {
             </tr>
         </thead>
         <tbody>
+        {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     );

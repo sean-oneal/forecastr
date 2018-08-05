fb0bc8d75b419ddef08ac0ddb5e60d8532ba7dce
Sean O'Neal <17163675+sean-oneal@users.noreply.github.com>
2018-08-04 18:05:14 -0700

    Add Averages to Chart, Calculate Average

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,12 +1,19 @@
+import _ from 'lodash';
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
-export default (props) => {
+export default ({ data, color }) => {
+  function calculateAverages (data) {
+    return Math.round( _.sum(data) / data.length);
+  }
+
   return (
     <div>
-     <Sparklines height={120} width={180} data={props.data} >
-        <SparklinesLine color={props.color} />
+     <Sparklines height={120} width={180} data={data} >
+        <SparklinesLine color={color} />
+        <SparklinesReferenceLine type="avg" />
       </Sparklines>
+        <div>{calculateAverages(data)}</div>
     </div>
   );
 }

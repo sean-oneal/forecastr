0eceb19f3305dbe294a40acaafcc3388ce31d035
Sean O'Neal <17163675+sean-oneal@users.noreply.github.com>
2018-08-04 18:07:46 -0700

    Add Measurement Units to Column Names

--- src/containers/weatherList.js
+++ src/containers/weatherList.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
 
   renderWeather(cityData) {
     const { name } = cityData.city;
     const temps = cityData.list.map(weatherData => weatherData.main.temp);
+    const pressures = cityData.list.map(weatherData => weatherData.main.pressure);
+    const humidities = cityData.list.map(weatherData => weatherData.main.humidity);
 
     return (
       <tr key={name}>
         <td>{name}</td>
         <td>
-          <Sparklines height={120} width={180} data={temps} >
-            <SparklinesLine color="blue" />
-          </Sparklines>
-          </td>
+          <Chart data={temps} color="blue" />
+        </td>
+        <td>
+          <Chart data={pressures} color="green" />
+        </td>
+        <td>
+          <Chart data={humidities} color="red" />
+        </td>
       </tr>
     );
   }
@@ -26,9 +32,9 @@ class WeatherList extends Component {
         <thead>
             <tr>
               <th>City</th>
-              <th>Temperature</th>
-              <th>Pressure</th>
-              <th>Humidity</th>
+              <th>Temperature (K)</th>
+              <th>Pressure (hPa)</th>
+              <th>Humidity (%)</th>
             </tr>
         </thead>
         <tbody>
